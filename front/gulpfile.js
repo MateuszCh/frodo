4b7a0bbe22861f2b967e27d00fb7732df91aa393
MateuszCh <mateusz.chybiorz@gmail.com>
2018-11-26 22:10:41 +0100

    npm audit fixes

--- front/gulpfile.js
+++ front/gulpfile.js
@@ -1,62 +1,62 @@
-const gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    inject = require("gulp-inject"),
-    hash = require("gulp-hash"),
-    imagemin = require("gulp-imagemin"),
-    uglify = require("gulp-uglify"),
-    prefix = require("gulp-autoprefixer"),
-    browserSync = require("browser-sync").create(),
-    concat = require("gulp-concat"),
-    concatCss = require("gulp-concat-css"),
-    htmlmin = require("gulp-htmlmin"),
-    cleancss = require("gulp-clean-css"),
-    series = require("stream-series"),
-    rename = require("gulp-rename"),
-    del = require("del");
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    inject = require('gulp-inject'),
+    hash = require('gulp-hash'),
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    prefix = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    htmlmin = require('gulp-htmlmin'),
+    cleancss = require('gulp-clean-css'),
+    series = require('stream-series'),
+    rename = require('gulp-rename'),
+    del = require('del');
 
 const paths = {
-    srcHTML: "src/**/*.html",
-    srcTemplates: "src/html/**/*.html",
-    srcSCSS: "src/sass/main.scss",
-    srcSCSSs: "src/sass/**/*.scss",
-    srcJS: "src/js/**/*.js",
-    srcIMAGES: "src/images/**/*",
+    srcHTML: 'src/**/*.html',
+    srcTemplates: 'src/html/**/*.html',
+    srcSCSS: 'src/sass/main.scss',
+    srcSCSSs: 'src/sass/**/*.scss',
+    srcJS: 'src/js/**/*.js',
+    srcIMAGES: 'src/images/**/*',
 
-    srcMaterialCss: "node_modules/angular-material/angular-material.min.css",
+    srcMaterialCss: 'node_modules/angular-material/angular-material.min.css',
 
-    public: "public",
-    publicIndex: "public/index.html",
-    publicCSS: "public/css",
-    publicJS: "public/js",
-    publicHTML: "public/html",
-    publicIMAGES: "public/images"
+    public: 'public',
+    publicIndex: 'public/index.html',
+    publicCSS: 'public/css',
+    publicJS: 'public/js',
+    publicHTML: 'public/html',
+    publicIMAGES: 'public/images'
 };
 
-const vendor = require("./vendor");
+const vendor = require('./vendor');
 
 const opts = {
-    algorithm: "sha1",
+    algorithm: 'sha1',
     hashLength: 40,
-    template: "<%= name %><%= ext %>?hash=<%= hash %>"
+    template: '<%= name %><%= ext %>?hash=<%= hash %>'
 };
 
 function errorLog(error) {
     console.error.bind(error);
-    this.emit("end");
+    this.emit('end');
 }
 
 ///////////
 // TASKS //
 ///////////
 
-gulp.task("html", function() {
+gulp.task('html', function() {
     return gulp
         .src(paths.srcHTML)
         .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest(paths.public));
 });
 
-gulp.task("htmlWatch", function() {
+gulp.task('htmlWatch', function() {
     return gulp
         .src(paths.srcTemplates)
         .pipe(htmlmin({ collapseWhitespace: true }))
@@ -64,91 +64,105 @@ gulp.task("htmlWatch", function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task("css", function() {
+gulp.task('css', function() {
     return gulp
         .src(paths.srcSCSS)
         .pipe(sass())
-        .on("error", errorLog)
-        .pipe(prefix("> 1%"))
-        .pipe(concatCss("main.css"))
+        .on('error', errorLog)
+        .pipe(prefix('> 1%'))
+        .pipe(concatCss('main.css'))
         .pipe(cleancss())
         .pipe(gulp.dest(paths.publicCSS))
         .pipe(browserSync.stream());
 });
 
-gulp.task("cssLib", function() {
+gulp.task('cssLib', function() {
     return gulp
         .src(paths.srcMaterialCss)
-        .pipe(rename("libs.min.css"))
+        .pipe(rename('libs.min.css'))
         .pipe(gulp.dest(paths.publicCSS));
 });
 
-gulp.task("js", function() {
+gulp.task('js', function() {
     return gulp
         .src(paths.srcJS)
-        .on("error", errorLog)
-        .pipe(concat("app.min.js"))
+        .on('error', errorLog)
+        .pipe(concat('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest(paths.publicJS))
         .pipe(browserSync.stream());
 });
 
-gulp.task("jsLib", function() {
+gulp.task('jsLib', function() {
     return gulp
         .src(vendor)
-        .on("error", errorLog)
-        .pipe(concat("libs.min.js"))
+        .on('error', errorLog)
+        .pipe(concat('libs.min.js'))
         .pipe(gulp.dest(paths.publicJS));
 });
 
-gulp.task("images", function() {
+gulp.task('images', function() {
     return gulp
         .src(paths.srcIMAGES)
         .pipe(imagemin())
         .pipe(gulp.dest(paths.publicIMAGES));
 });
 
-gulp.task("copy", ["html", "css", "js", "images"]);
+gulp.task('copy', gulp.series(gulp.parallel('html', 'css', 'js', 'images')));
 
-gulp.task("inject", ["copy"], function() {
-    const css = gulp.src(["public/css/main.css"], { read: false });
-    const vendorCss = gulp.src(["public/css/libs.min.css"], { read: false });
-    const vendorStream = gulp.src(["public/js/libs.min.js"], { read: false });
-    const appStream = gulp.src(["public/js/app.min.js"], { read: false });
+gulp.task(
+    'inject',
+    gulp.series(gulp.parallel('copy'), function() {
+        const css = gulp.src(['public/css/main.css'], { read: false });
+        const vendorCss = gulp.src(['public/css/libs.min.css'], {
+            read: false
+        });
+        const vendorStream = gulp.src(['public/js/libs.min.js'], {
+            read: false
+        });
+        const appStream = gulp.src(['public/js/app.min.js'], { read: false });
 
-    return gulp
-        .src(paths.publicIndex)
-        .pipe(
-            inject(series(vendorStream, appStream).pipe(hash(opts)), {
-                relative: true
-            })
-        )
-        .pipe(
-            inject(series(vendorCss, css).pipe(hash(opts)), { relative: true })
-        )
-        .pipe(gulp.dest(paths.public));
-});
+        return gulp
+            .src(paths.publicIndex)
+            .pipe(
+                inject(series(vendorStream, appStream).pipe(hash(opts)), {
+                    relative: true
+                })
+            )
+            .pipe(
+                inject(series(vendorCss, css).pipe(hash(opts)), {
+                    relative: true
+                })
+            )
+            .pipe(gulp.dest(paths.public));
+    })
+);
 
-// Browser-Sync Task
-gulp.task("browser-sync", ["inject"], function() {
-    browserSync.init({
-        port: 3002,
-        proxy: {
-            target: "localhost:3000",
-            ws: false
-        }
-    });
-});
+gulp.task(
+    'browser-sync',
+    gulp.series(gulp.parallel('inject'), function() {
+        browserSync.init({
+            port: 3002,
+            proxy: {
+                target: 'localhost:3000',
+                ws: false
+            }
+        });
+    })
+);
 
-gulp.task("watch", ["browser-sync"], function() {
-    gulp.watch([paths.srcTemplates], ["htmlWatch"]);
-    gulp.watch([paths.srcSCSSs], ["css"]);
-    gulp.watch([paths.srcJS], ["js"]);
-});
+gulp.task(
+    'watch',
+    gulp.series(gulp.parallel('browser-sync'), function() {
+        gulp.watch([paths.srcTemplates], ['htmlWatch']);
+        gulp.watch([paths.srcSCSSs], ['css']);
+        gulp.watch([paths.srcJS], ['js']);
+    })
+);
 
-gulp.task("default", ["cssLib", "jsLib", "inject"]);
+gulp.task('default', gulp.series(gulp.parallel('cssLib', 'jsLib', 'inject')));
 
-gulp.task("clean", function() {
+gulp.task('clean', function() {
     del([
         paths.publicIndex,
         paths.publicHTML,
